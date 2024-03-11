7dc1d931005e6df7d2d2a6bde12123aacf8b5691
kaspernux <114181059+kaspernux@users.noreply.github.com>
2024-03-11 06:08:11 +0000

    new file:   .bin/apache-container.sh 	deleted:    .bin/nginx-container.sh 	new file:   apache.conf 	modified:   docker/Dockerfile

--- .bin/apache-container.sh
+++ .bin/apache-container.sh
@@ -1,4 +1,4 @@
 # container id
-CONTAINER_ID=$(docker ps -aqf "name=gab-nginx")
+CONTAINER_ID=$(docker ps -aqf "name=gab-php-apache")
 
 docker exec -w /var/www/html/gab-app -it ${CONTAINER_ID} bash
