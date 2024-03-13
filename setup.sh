cee4832cba0611a4922c95c35abfc31fb434a24b
kaspernux <114181059+kaspernux@users.noreply.github.com>
2024-03-13 05:14:37 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -305,11 +305,12 @@ echo "DB_PASSWORD=${MYSQL_LARAVEL_PASSWORD}" >> /var/www/html/gab-app/.env
 # Update Apache configuration to point to the Laravel app
 sudo sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/html/gab-app/public|g' /etc/apache2/sites-available/gab-app.conf
 
-# Restart Apache to apply changes
-sudo systemctl restart apache2
 
-# Configure Apache virtual host with user-provided domain or IP address
-cat <<EOF > /etc/apache2/sites-available/gab-app.conf
+# Configure Apache virtual host for your Laravel project
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/gab-app.conf
+
+# Edit Apache virtual host configuration file
+sudo tee /etc/apache2/sites-available/gab-app.conf > /dev/null <<EOF
 <VirtualHost *:80>
     ServerName $server_domain
     ServerAdmin webmaster@localhost
